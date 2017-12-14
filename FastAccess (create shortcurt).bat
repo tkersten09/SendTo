f50d535d93429ccf98278f32dd79c509f085f437
tkersten09 <34385640+tkersten09@users.noreply.github.com>
2017-12-14 19:03:27 +0100

    Initial commit

---
+++ FastAccess (create shortcurt).bat
@@ -0,0 +1,7 @@
+set TARGET='%1'
+set SHORTCUT='D:\Thomas\FastAccess\test.lnk'
+set PWS=powershell.exe -ExecutionPolicy Bypass -NoLogo -NonInteractive -NoProfile
+
+%PWS% -Command "$ws = New-Object -ComObject WScript.Shell; $s = $ws.CreateShortcut(%SHORTCUT%); $S.TargetPath = %TARGET%; $S.Save()"
+
+timeout 100
